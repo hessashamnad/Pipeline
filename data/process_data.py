205ea4020053410d90e91f2e2bb57103c2894966
hessashamnad <hessa.shamnad@gmail.com>
2020-07-24 14:21:18 -0500

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,62 @@
 import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Load and merge messages and categories datasets
+    Input:
+        messages_filepath: File path of messages data
+        categories_filepath: File path of categories data
+    Output:
+        df: Merged dataset from messages and categories
+    '''
+    #Read message csv
+    messages = pd.read_csv(messages_filepath)
+    # Read categories csv
+    categories = pd.read_csv(categories_filepath)
+    
+    # Merge messages and categories
+    df = pd.merge(messages, categories, on='id')
+    
+    return df
 
+    
 
 def clean_data(df):
-    pass
-
+    '''
+    Input:
+        df: Merged dataset from messages and categories
+    Output:
+        df: Cleaned dataset
+    '''
+    categories = df.categories.str.split(pat=';',expand=True)
+    firstrow = categories.iloc[0]
+    category_col = firstrow.apply(lambda x:x[:-2])
+    categories.columns = category_col
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(np.int)
+    df = df.drop('categories',axis=1)
+    df = pd.concat([df,categories],axis=1)
+    df = df.drop_duplicates()
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save into  SQLite database.
+    
+    inputs:
+    df: Dataframe that includes cleaned version of merged message and 
+    categories data.
+    database_filename: Filename for output database.
+   
+    """
+    engine = create_engine('sqlite:///data//DisasterResponse.db')
+    df.to_sql('messages_disaster', engine, index=False)
+    
 
 
 def main():
@@ -31,8 +77,8 @@ def main():
         print('Cleaned data saved to database!')
     
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
+        print('Please provide the filepaths of the messages & categories '\
+              'datasets as the first & second argument respectively, as '\
               'well as the filepath of the database to save the cleaned data '\
               'to as the third argument. \n\nExample: python process_data.py '\
               'disaster_messages.csv disaster_categories.csv '\
