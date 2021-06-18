e4b2173dc6aac992343b349ba44c204f03114578
hessashamnad <hessa.shamnad@gmail.com>
2021-06-18 10:14:38 -0500

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,7 +1,6 @@
 import json
 import plotly
 import pandas as pd
-import numpy as np
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -9,6 +8,7 @@ from nltk.tokenize import word_tokenize
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
+from plotly.graph_objs import Heatmap
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
@@ -28,13 +28,13 @@ def tokenize(text):
 
 # load data
 engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('messages_disaster', engine)
+df = pd.read_sql_table('disaster_response', engine)
 
 # load model
 model = joblib.load("../models/classifier.pkl")
 
 
-# # index webpage displays cool visuals and receives user input text for model
+# index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
@@ -43,8 +43,12 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    cat_count = df.iloc[:,5:].sum().sort_values(ascending=False)[1:6]
-    cat_names = list(cat_count.index)
+    y = df.drop(['id', 'message', 'original', 'genre'], axis=1).astype(float)
+    cat_names = y.columns.values
+    cat_counts = [sum(y[x]) for x in cat_names]
+    corr = y.corr()
+    labels = y.columns.values
+    
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -66,119 +70,16 @@ def index():
                 }
             }
         },
-
         {
             'data': [
                 Bar(
                     x=cat_names,
-                    y=cat_count
+                    y=cat_counts
                 )
             ],
 
             'layout': {
-                'title': 'Top Categories',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Categories"
-                }
-            }
-        }
-    ]
-    
-    
-    # encode plotly graphs in JSON
-    ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
-    # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJSON)
-
-
-# web page that handles user query and displays model results
-@app.route('/go')
-def go():
-    # save user input in query
-    query = request.args.get('query', '') 
-
-    # use model to predict classification for query
-    classification_labels = model.predict([query])[0]
-    classification_results = dict(zip(df.columns[4:], classification_labels))
-
-    # This will render the go.html Please see that file. 
-    return render_template(
-        'go.html',
-        query=query,
-        classification_result=classification_results
-    )
-
-
-def main():
-    app.run(host='0.0.0.0', port=3001, debug=True)
-
-
-if __name__ == '__main__':
-    main()import json
-import plotly
-import pandas as pd
-import numpy as np
-
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
-from flask import Flask
-from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
-from sklearn.externals import joblib
-from sqlalchemy import create_engine
-
-
-app = Flask(__name__)
-
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
-
-    clean_tokens = []
-    for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-        clean_tokens.append(clean_tok)
-
-    return clean_tokens
-
-# load data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('messages_disaster', engine)
-
-# load model
-model = joblib.load("../models/classifier.pkl")
-
-
-# # index webpage displays cool visuals and receives user input text for model
-@app.route('/')
-@app.route('/index')
-def index():
-    
-    # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
-    genre_names = list(genre_counts.index)
-    cat_count = df.iloc[:,5:].sum().sort_values(ascending=False)[1:6]
-    cat_names = list(cat_count.index)
-    # create visuals
-    # TODO: Below is an example - modify to create your own visuals
-    graphs = [
-        {
-            'data': [
-                Bar(
-                    x=genre_names,
-                    y=genre_counts
-                )
-            ],
-
-            'layout': {
-                'title': 'Distribution of Message Genres',
+                'title': 'Distribution of Message categories',
                 'yaxis': {
                     'title': "Count"
                 },
@@ -187,27 +88,21 @@ def index():
                 }
             }
         },
-
         {
             'data': [
-                Bar(
-                    x=cat_names,
-                    y=cat_count
+                Heatmap(
+                    z=corr.values,
+                    x=labels,
+                    y=labels
                 )
             ],
 
             'layout': {
-                'title': 'Top Categories',
-                'yaxis': {
-                    'title': "Count"
-                },
-                'xaxis': {
-                    'title': "Categories"
-                }
+                'title': 'Categories Correlation',
+                'height': 1000
             }
         }
     ]
-    
     
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
